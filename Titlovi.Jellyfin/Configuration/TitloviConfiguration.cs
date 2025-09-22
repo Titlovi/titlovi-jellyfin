0cb310ca63030185a4ada9c5b3f71d35c0bd419a
maatko <matkovukovic25@gmail.com>
2025-09-22 21:55:17 +0200

    added base plugin class & simple configuration page

--- Titlovi.Jellyfin/Configuration/TitloviConfiguration.cs
+++ Titlovi.Jellyfin/Configuration/TitloviConfiguration.cs
@@ -4,4 +4,15 @@ namespace Titlovi.Jellyfin.Configuration;
 
 public class TitloviConfiguration : BasePluginConfiguration
 {
+    /// <summary>
+    /// Gets or sets a username used for authenticating
+    /// with the Titlovi api endpoint.
+    /// </summary>
+    public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets a password used for authenticating
+    /// with the Titlovi api endpoint.
+    /// </summary>
+    public string Password { get; set; } = string.Empty;
 }
